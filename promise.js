8bd9aa8ea6a442b522a904259a80df0e45831d5d
Bukan Bang Toyib <nyobagan@yahoo.com>
2022-09-20 13:29:44 +0700

    feat: nyoba lagi

--- promise.js
+++ promise.js
@@ -2,22 +2,17 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emotion) => {
-
   const theaterIXX = await promiseTheaterIXX();
   const theaterVGC = await promiseTheaterVGC();
 
-  const allTheater = [...theaterIXX, ...theaterVGC]
-
-  let countE = 0;
-
-  allTheater.forEach((film) => {
-    if (film.hasil === emotion) {
-      countE++;
+  const theaters = [...theaterIXX, ...theaterVGC];
+  let count = 0;
+  theaters.forEach((item) => {
+    if(item.hasil == emotion){
+      count++;
     }
   })
-
-  return countE;
-
+  return count;
 };
 
 module.exports = {
